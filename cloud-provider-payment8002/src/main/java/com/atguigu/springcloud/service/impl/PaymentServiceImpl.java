de5d5e0522e0417548595d4dc86d9de1bc403b9e
zhuzhaoyang <535983654@qq.com>
2021-04-20 23:58:39 +0800

    <类型> : 添加  <描述> : 添加eureka 服务loadblance  [正文] :  [脚注] :

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.dao.PaymentDao;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        int create = paymentDao.create(payment);
+        return create;
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        Payment payment = paymentDao.getPaymentById(id);
+        return payment;
+    }
+}
